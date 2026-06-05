1edd650ce3eab1ceea9c777305fa356803547259
juan <juanillogarcia14@gmail.com>
2026-06-05 12:57:58 +0200

    feat(http): implement http interceptors for jwt

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideStore } from '@ngxs/store';
@@ -7,12 +7,16 @@ import Nora from '@primeuix/themes/nora';
 
 import { routes } from './app.routes';
 import { AppStates } from './store';
+import { authInterceptor } from './core/interceptors/auth.interceptor';
+import { refreshTokenInterceptor } from './core/interceptors/refresh-token.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideStore(AppStates),
-    provideHttpClient(),
+    provideHttpClient(
+      withInterceptors([authInterceptor, refreshTokenInterceptor]),
+    ),
     // PrimeNG theme preset + dark mode via CSS class toggle
     providePrimeNG({
       ripple: true,
